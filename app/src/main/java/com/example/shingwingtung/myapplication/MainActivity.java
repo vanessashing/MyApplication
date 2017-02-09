2a4f23388a528d616815a1106dcdf3817011be4c
Shing Wing Tung <shingwingtung@ShingdeMacBook-Air.local>
2017-02-09 09:24:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shingwingtung/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.shingwingtung.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        forceError();
+    }
+
+    public void forceError() {
+        if (true) {
+            throw new Error("Whoops");
+        }
+    }
+}
